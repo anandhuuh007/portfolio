76e222a4c2f13b1ab06dae50a03b70c1d85f2053
anandhuuh007 <anandhucectl.ac.in@gmail.com>
2025-06-20 19:35:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+  VANTA.NET({
+    el: "#vanta-bg",  // The container div ID
+    color: 0x00d1ff,  // Line color in hex (customize as you like)
+    backgroundColor: 0x121212,  // Background color (match your site bg)
+    points: 12.00,
+    maxDistance: 20.00,
+    spacing: 15.00,
+    showDots: true,
+  });
+</script>
