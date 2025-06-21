942e566dec32c623f4f081aa99de5e7ed74bf7f8
anandhuuh007 <anandhucectl.ac.in@gmail.com>
2025-06-21 09:15:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 <script>
   VANTA.NET({
-    el: "#vanta-bg",  // The container div ID
-    color: 0x00d1ff,  // Line color in hex (customize as you like)
-    backgroundColor: 0x121212,  // Background color (match your site bg)
+    el: "#vanta-bg",  
+    color: 0x00d1ff,  
+    backgroundColor: 0x121212,  
     points: 12.00,
     maxDistance: 20.00,
     spacing: 15.00,
