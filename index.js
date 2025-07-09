aa6bd462edce8005dcd5e1cb193b78258eda1714
Anandhu <anandhucectl.ac.in@gmail.com>
2025-07-09 20:10:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+  VANTA.NET({
+    el: "#vanta-bg",  
+    color: 0x00d1ff,  
+    backgroundColor: 0x121212,  
+    points: 12.00,
+    maxDistance: 20.00,
+    spacing: 15.00,
+    showDots: true,
+  });
+</script>
